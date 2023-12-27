aeb0c618611257b4a365bd1abb92c460eb4981cd
xiaozhou26 <xiaoxiaofeihh@foxmail.com>
2023-12-27 16:49:38 +0800

    up

--- upload.js
+++ upload.js
@@ -1,52 +1,65 @@
-document.getElementById('upload-form').addEventListener('submit', function(event) {
-  event.preventDefault();
+document
+  .getElementById("upload-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
 
-  // Your GitHub token
-  const token = process.env.token;
+    // Your GitHub token
+    const token = "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";
 
-  // The name of your repository
-  const repo = process.env.repo;
+    // The name of your repository
+    const repo = "xiaozhou26/tuph";
 
-  // The name of the branch
-  const branch = process.env.branch || "main";
+    // The name of the branch
+    const branch = "main";
 
-  // The path where the image will be stored
-  const path = process.env.path || "/images";
+    // The path where the image will be stored
+    const path = 'images';
 
     // The name of the image file
-    const filename = document.getElementById('fileToUpload').files[0].name;
+    const filename = document.getElementById("fileToUpload").files[0].name;
 
     // The image file
-    const file = document.getElementById('fileToUpload').files[0];
+    const file = document.getElementById("fileToUpload").files[0];
 
     // Create a new FileReader instance
     const reader = new FileReader();
 
-    reader.onloadend = function() {
+    reader.onloadend = function () {
       // The file's content as a Base64 string
-      const base64String = reader.result.replace(/^data:.+;base64,/, '');
+      const base64String = reader.result.replace(/^data:.+;base64,/, "");
 
       // Send a POST request to the GitHub API
-      fetch(`https://api.github.com/repos/${repo}/contents/${path}${filename}?branch=${branch}`, {
-        method: 'PUT',
-        headers: {
-          'Authorization': `token ${token}`
-        },
-        body: JSON.stringify({
-          message: 'upload image',
-          content: base64String
+      fetch(
+        `https://api.github.com/repos/${repo}/contents/${path}${filename}?branch=${branch}`,
+        {
+          method: "PUT",
+          headers: {
+            Authorization: `token ${token}`,
+          },
+          body: JSON.stringify({
+            message: "upload image",
+            content: base64String,
+          }),
+        }
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          // The URL of the image
+          let imageUrl = data.content.download_url;
+
+          imageUrl = imageUrl
+            .replace(/(?<=com\/.+?\/.+?)\/(.+?\/)/, "@$1")
+            .replace(
+              /^(?:https?:\/\/)?raw\.(?:githubusercontent|github)\.com/,
+              "https://jsd.cdn.zzko.cn/gh"
+            );
+
+          // Display the image URL
+          document.getElementById("image-url").textContent = imageUrl;
         })
-      }).then(res => res.json()).then(data => {
-        // The URL of the image
-        let imageUrl = data.content.download_url;
-
-        imageUrl = imageUrl.replace(/(?<=com\/.+?\/.+?)\/(.+?\/)/, '@$1').replace(/^(?:https?:\/\/)?raw\.(?:githubusercontent|github)\.com/, 'https://jsd.cdn.zzko.cn/gh');
-
-        // Display the image URL
-        document.getElementById('image-url').textContent = imageUrl;
-      }).catch(error => {
-        console.error(error);
-      });
+        .catch((error) => {
+          console.error(error);
+        });
     };
 
     reader.readAsDataURL(file);
