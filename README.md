80da683a0b695c8a6366ab91b68b809452e9e25c
xiaozhou26 <xiaoxiaofeihh@foxmail.com>
2024-02-17 21:26:28 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# gh-photo
+## 简介
+可以存一些图片到GitHub，并用开源Chinajsdelivr进行加速在全球快速访问
+## 使用
+### 前往 [Glitch](https://glitch.com/) 注册账户，然后点击右边按钮 =====> [![Remix on Glitch](https://cdn.glitch.com/2703baf2-b643-4da7-ab91-7ee2a2d00b5b%2Fremix-button.svg)](https://glitch.com/edit/#!/import/github/xiaozhou26/gh-photo* )
+### 依照upload.js中的注释填写
+### ![](https://jsd.cdn.zzko.cn/gh/xiaozhou26/tuph@main/20231222194210.png)
+目前纯静态只能明文保存，希望有大佬带带。
+## 部署成功
+这样你旁边就可以看到链接了
+## 请大家积极pull和提issue
+## 鸣谢https://github.com/54ayao/Chinajsdelivr
+为我们提供了国内加速服务，请大家合规使用，感谢。
---
+++ upload.js
@@ -0,0 +1,66 @@
+document
+  .getElementById("upload-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    // Your GitHub token
+    const token = "";
+
+    // The name of your repository
+    const repo = "xiaozhou26/tuph";
+
+    // The name of the branch
+    const branch = "main";
+
+    // The path where the image will be stored
+    const path = 'images';
+
+    // The name of the image file
+    const filename = document.getElementById("fileToUpload").files[0].name;
+
+    // The image file
+    const file = document.getElementById("fileToUpload").files[0];
+
+    // Create a new FileReader instance
+    const reader = new FileReader();
+
+    reader.onloadend = function () {
+      // The file's content as a Base64 string
+      const base64String = reader.result.replace(/^data:.+;base64,/, "");
+
+      // Send a POST request to the GitHub API
+      fetch(
+        `https://api.github.com/repos/${repo}/contents/${path}${filename}?branch=${branch}`,
+        {
+          method: "PUT",
+          headers: {
+            Authorization: `token ${token}`,
+          },
+          body: JSON.stringify({
+            message: "upload image",
+            content: base64String,
+          }),
+        }
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          // The URL of the image
+          let imageUrl = data.content.download_url;
+
+          imageUrl = imageUrl
+            .replace(/(?<=com\/.+?\/.+?)\/(.+?\/)/, "@$1")
+            .replace(
+              /^(?:https?:\/\/)?raw\.(?:githubusercontent|github)\.com/,
+              "https://jsd.cdn.zzko.cn/gh"
+            );
+
+          // Display the image URL
+          document.getElementById("image-url").textContent = imageUrl;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    };
+
+    reader.readAsDataURL(file);
+  });
